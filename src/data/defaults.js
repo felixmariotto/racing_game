f7ff97d393a56ddcf88b8f85b5897b91d04bc7eb
felixmariotto <46470486+felixmariotto@users.noreply.github.com>
2020-09-03 23:14:06 +0200

    update a bit the camera settings

--- src/data/defaults.js
+++ src/data/defaults.js
@@ -3,9 +3,9 @@ import * as THREE from 'three';
 
 export default {
 	sceneBackColor: 0x505050,
-	cameraFOV: 75,
+	cameraFOV: 90,
 	cameraMinPlane: 0.04,
-	cameraFarPlane: 10,
-	cameraInitPos: new THREE.Vector3( 0, 1.6, 0 ),
-	cameraInitTarget: new THREE.Vector3( 0, 1, -1.8 )
+	cameraFarPlane: 100,
+	cameraInitPos: new THREE.Vector3( 0, 1, 5 ),
+	cameraInitTarget: new THREE.Vector3( 0, 0, 0 )
 }
